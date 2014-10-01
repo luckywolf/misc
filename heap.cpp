23328c2ae89d3c0f348dfefc6ba95597fe43835b
luckywolf <zhou4car@gmail.com>
2014-10-01 12:04:10 -0700

    another version

--- heap.cpp
+++ heap.cpp
@@ -7,12 +7,13 @@ public:
     void insert(T t);
     T getMin();
 };
-
+/*  version 1 */
 class intHeap : public heap<int> {
 public:    
     intHeap(int m): _maxSize(m), _size(0), _vec(m) {}
 
     void insert(int val) {
+        assert(_size > 0);
         int i, p;
         _vec[_size++] = val;
         for (int i = _size-1; i > 0 && _vec[p = (i-1)/2] > _vec[i]; i = p) {
@@ -38,6 +39,47 @@ public:
     }
 private:
     int _maxSize, _size;
+    vector<int> _vec;
+    swap(int i, int j) {
+        int temp = _vec[i];
+        _vec[i] = _vec[j];
+        _vec[j] = temp;
+    }
+};
+
+/* version 2 */
+class intHeap : public heap<int> {
+public:    
+    intHeap(int m): _maxSize(m), _size(0), _vec() {}
+
+    void insert(int val) {
+        assert(_vec.size() > 0);
+        int i, p;
+        _vec.push_back(val);
+        for (int i = _vec.size()-1; i > 0 && _vec[p = (i-1)/2] > _vec[i]; i = p) {
+            swap(p, i);
+        }
+    }
+    
+    int getMin() {
+        assert(_vec.size() > 0);
+        int i, c;
+        int res = _vec[0];
+        _vec[0] = _vec.back();
+        vec.pop_back();
+        for (int i = 0; (c = 2*i+1) < _vec.size(); i = c) {
+            if (c+1 < _vec.size() && _vec[c+1] < _vec[c]) {
+                ++c;
+            }
+            if (_vec[i] <= _vec[c]) {
+                break;
+            }
+            swap(c, i);
+        }
+        return res;
+    }
+private:
+    int _maxSize;
     vector<int> _vec;
     swap(int i, int j) {
         int temp = _vec[i];
