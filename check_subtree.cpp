d9a9e2732087f63ea7dd95fc1d7ec73f55bec5c7
luckywolf <zhou4car@gmail.com>
2014-10-26 20:05:09 -0700

    Update check_subtree.cpp

--- check_subtree.cpp
+++ check_subtree.cpp
@@ -2,7 +2,9 @@
 http://www.geeksforgeeks.org/check-if-a-binary-tree-is-subtree-of-another-binary-tree/
 
 Check if a binary tree is subtree of another binary tree | Set 1
-Given two binary trees, check if the first tree is subtree of the second one. A subtree of a tree T is a tree S consisting of a node in T and all of its descendants in T. The subtree corresponding to the root node is the entire tree; the subtree corresponding to any other node is called a proper subtree.
+Given two binary trees, check if the first tree is subtree of the second one. A subtree of a tree T is a tree S 
+consisting of a node in T and all of its descendants in T. The subtree corresponding to the root node is the entire 
+tree; the subtree corresponding to any other node is called a proper subtree.
 
 For example, in the following case, tree S is a subtree of tree T.
 
