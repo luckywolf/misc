dd373f7e516059580c998dfa737397d6e9e9c4d2
luckywolf <zhou4car@gmail.com>
2014-10-26 20:04:48 -0700

    Update check_bipartite_graph.cpp

--- check_bipartite_graph.cpp
+++ check_bipartite_graph.cpp
@@ -4,9 +4,10 @@ www.geeksforgeeks.org/bipartite-graph/
 Following is a simple algorithm to find out whether a given graph is Birpartite or not using Breadth First Search (BFS).
 1.	Assign RED color to the source vertex (putting into set U).
 2.	Color all the neighbors with BLUE color (putting into set V).
-3.	Color all neighbor¡¯s neighbor with RED color (putting into set U).
+3.	Color all neighborÂ¡Â¯s neighbor with RED color (putting into set U).
 4.	This way, assign color to all vertices such that it satisfies all the constraints of m way coloring problem where m = 2.
-5. While assigning colors, if we find a neighbor which is colored with same color as current vertex, then the graph cannot be colored with 2 vertices (or graph is not Bipartite)
+5. While assigning colors, if we find a neighbor which is colored with same color as current vertex, then the graph cannot 
+be colored with 2 vertices (or graph is not Bipartite)
 */
 
 #include <iostream>
